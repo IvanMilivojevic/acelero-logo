74afba23c119e410fae06a9be51e2d3b6143346c
Ivan Milivojevic <seltik89@hotmail.com>
2019-12-22 23:35:53 +0100

    Project transfer to github

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function(){
+	setTimeout(function(){
+		jQuery(".anim-holder").addClass("active-mask");
+	}, 400);
+	setTimeout(function(){
+		jQuery(".big-anim").addClass("active");
+	}, 1400);
+});
